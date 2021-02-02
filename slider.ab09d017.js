4af9e9f04e30c68c9aa39011abddf7e1b9756373
Anna <72921518+AnnaIzhovska@users.noreply.github.com>
2021-02-02 09:57:57 +0200

    Updates

--- slider.ab09d017.js
+++ slider.ab09d017.js
@@ -121,6 +121,23 @@ parcelRequire = (function (modules, cache, entry, globalName) {
 $(document).ready(function () {
   $('.slider').slick();
 });
+$(document).ready(function () {
+  $('.slider2').slick();
+});
+$('.slider').slick({
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  // arrows: false,
+  // fade: true,
+  asNavFor: '.slider2'
+});
+$('.slider2').slick({
+  slidesToShow: 3,
+  slidesToScroll: 1,
+  asNavFor: '.slider' // centerMode: true,
+  // focusOnSelect: true
+
+});
 },{}],"../node_modules/parcel-bundler/src/builtins/hmr-runtime.js":[function(require,module,exports) {
 var global = arguments[3];
 var OVERLAY_ID = '__parcel__error__overlay__';
@@ -149,7 +166,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "51601" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "51961" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
